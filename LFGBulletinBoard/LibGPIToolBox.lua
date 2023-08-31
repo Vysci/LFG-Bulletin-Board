885110d7497ddf5e052315f847bb10a2286033d4
Zack Chadwick <zack.chadwick@gmail.com>
2023-08-30 22:46:08 -0400

    Update for Classic Era 1.14.4

--- LFGBulletinBoard/LibGPIToolBox.lua
+++ LFGBulletinBoard/LibGPIToolBox.lua
@@ -343,7 +343,8 @@ local SizingEnter=function(self)
 	if not (GetCursorInfo()) then
 		ResizeCursor:Show()
 		ResizeCursor.Texture:SetTexture(self.GPI_Cursor)
-		ResizeCursor.Texture:SetRotation(math.rad(self.GPI_Rotation),CreateVector2D(0.5,0.5))
+		--Commenting this out to avoid an LUA error. Has to do with rotation of the resize cursor, can't figure it out at the moment
+		--ResizeCursor.Texture:SetRotation(math.rad(self.GPI_Rotation),CreateVector2D(0.5,0.5))
 	end
 end
 
