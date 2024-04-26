9cd7121a2bcfeefe5ff42d704ea06ae51ca7b4b0
Julio Ramirez <julio1031.jr@gmail.com>
2024-04-26 14:24:47 -0700

    fix: `SetClampedToScreen` to `true` for `GroupBulletinBoardFrame`
    prevents frame being dragged out of screen limit.

--- LFGBulletinBoard/GroupBulletinBoard.lua
+++ LFGBulletinBoard/GroupBulletinBoard.lua
@@ -398,7 +398,7 @@ end
 
 function GBB.Init()
 	GroupBulletinBoardFrame:SetResizeBounds(400,170)	
-	
+	GroupBulletinBoardFrame:SetClampedToScreen(true)
 	GBB.UserLevel=UnitLevel("player")
 	GBB.UserName=(UnitFullName("player"))
 	GBB.ServerName=GetRealmName()
